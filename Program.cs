5fa0d73a7572fb44864ca22c54f07e0f87487abf
Shawn Taylor <shawncurtistaylor@hotmail.com>
2020-03-25 06:13:20 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace build_your_own_universe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
